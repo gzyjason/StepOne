8a215fc4e0fd37dca88205cc26a223cea2e92f18
gzyjason <gzyjason@gmail.com>
2026-07-09 00:35:52 -0600

    Initial Commit

---
+++ StepOne/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StepOne
+//
+//  Created by Jason Gao on 7/9/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StepOne/StepOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepOneApp.swift
+//  StepOne
+//
+//  Created by Jason Gao on 7/9/26.
+//
+
+import SwiftUI
+
+@main
+struct StepOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
